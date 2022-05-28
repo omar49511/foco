b38be95bbcecae40b9c7e23b896dedce4538d424
omar49511 <72781778+omar49511@users.noreply.github.com>
2022-05-27 17:41:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector('.slider');
+let body = document.querySelector('body');
+
+btn.onclick = function(){
+    body.classList.toggle('on');
+    // let x_luz = Math.floor(Math.random()*256);
+    // let y_luz = Math.floor(Math.random()*256);
+    // let z_luz = Math.floor(Math.random()*256);
+
+    // luzColor= "rgb("+x_luz+","+y_luz+","+z_luz+")";
+    // let btn_f = document.querySelector('.foco');
+    // let cambiar = btn_1.style.background = luzColor;
+}
